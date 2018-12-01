0748d73618ae3abf86b7a501d00d89f34e85cce0
buxfix <buxfix@gmail.com>
2018-12-01 13:04:00 +0100

    update index.file

--- webpackdemo/webpack.config.js
+++ webpackdemo/webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            title: 'FuckUp Management'
+            title: 'BFA DemoPage'
         })
     ],
     output: {
