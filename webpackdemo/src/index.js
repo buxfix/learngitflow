5c9cdffd18b80ad4e9c600e95f98da747a183cb9
buxfix <buxfix@gmail.com>
2018-12-01 12:44:58 +0100

    htmlhelper

--- webpackdemo/src/index.js
+++ webpackdemo/src/index.js
@@ -1,9 +1,12 @@
 import _ from 'lodash';
-import './style.css';
+import printMe from './print.js';
+
 function component() {
     let element = document.createElement('div');
-    element.innerHTML = _.join(['Hello','webpack'],' ');
-    element.classList.add('hello');
+    let btn = document.createElement('button');    
+    btn.innerHTML = 'Click me &amp; check console';
+    btn.onClick = printMe;
+    element.appendChild(btn);
     return element;
 }
 
